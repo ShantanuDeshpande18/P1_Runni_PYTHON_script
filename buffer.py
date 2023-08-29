0236a57cf1203b67cf2a55782887d44099ca2ef6
ShantanuDeshpande18 <reachshantanu18@gmail.com>
2023-08-29 16:36:08 +0530

    Buffer Created

--- buffer.py
+++ buffer.py
@@ -1,3 +1,9 @@
 import arcpy
 
 arcpy.env.workspace = r"D:\MSC\sem_3\ProgGIS#3\Prac1_Running_Python\P1_Runni_PYTHON_script\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
+
+#performing buffer operation
+
+arcpy.analysis.Buffer("Wilson_Schools", "Wilson_Schools_Buffer_2", "500 meters")
+
+print("Proceess done")
