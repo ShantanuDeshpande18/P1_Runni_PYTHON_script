25983f03c7a066c89eea6be45409f2ad60150817
ShantanuDeshpande18 <reachshantanu18@gmail.com>
2023-08-29 14:25:51 +0530

    Converting int top string

--- trial.py
+++ trial.py
@@ -6,3 +6,13 @@ print(name)
 print(surname)
 
 print("My first name is {} and my surname is {}".format(name, surname))
+age = 22.5
+
+print(type(age))
+print(type(surname))
+
+# converting string into int
+
+result_integer =int(age)
+print(type(result_integer))
+
