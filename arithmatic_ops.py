06a8de1ab5f8fe9790a17a7c8a71381ab923d3c1
ShantanuDeshpande18 <reachshantanu18@gmail.com>
2023-09-04 13:09:15 +0530

    created a py file for arithmetic ops

---
+++ arithmatic_ops.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 36
+# Addition #
+
+add = num1 + num2
+print("Addition of the numbers {} and {} is {}".format(num1, num2, add))
+# SUBS #
+subs = num2 - num1
+
+# Multi #
+
+multi = num1 * num2
+print("Multi of {} and {} is {}".format(num1, num2, multi))
+
+#div#
+
+div = num2 / num1
+print("Div of {} and {} is {}".format(num2, num1, div))
